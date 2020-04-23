26e6a05112b4ea93865957a2082bf597a5f85413
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-04-23 20:36:53 +0530

    Stage 2

--- Zookeeper.py
+++ Zookeeper.py
@@ -1,6 +1,28 @@
 # Write your code here
+# Stage 1
 print("""I do love animals!
 Start looking after animals...
 Deer looks fine.
 Bat looks happy.
 Lion looks healthy.""")
+
+# Stage 2
+print("""Switching on camera from habitat with camels...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Yey, our little camel is sunbathing!""")
