a6f838c30c019dec08c0a1d6ec49e40f91b6360a
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-04-24 14:48:29 +0530

    Stage 2

--- Simple_Chatty_Bot.py
+++ Simple_Chatty_Bot.py
@@ -1,7 +1,17 @@
 # write your code here
 
-#Stage 1
+# Stage 1
 
 print('''
 Hello! My name is Aid.
 I was created in 2020.''')
+
+
+# Stage 2
+
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+yourName = input()
+print(f'What a great name you have, {yourName}!')
