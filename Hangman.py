0f665540ca9d57a56f7d5e3ff5dcaec40c1e2f16
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-04-25 09:15:22 +0530

    Stage 2 implemented successfully

--- Hangman.py
+++ Hangman.py
@@ -2,3 +2,13 @@
 
 print('''H A N G M A N
 The game will be available soon.''')
+
+
+# Stage 2
+
+print('H A N G M A N')
+name = input("Guess the word:")
+if name == "python":
+    print("You survived!")
+else:
+    print("You are hanged!")
