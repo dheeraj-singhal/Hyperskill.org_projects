0fec2fc3c67944e1bad91d86d6bbc3fec5dee968
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-04-29 11:08:55 +0530

    Stage 5 implemented successfully

--- Hangman.py
+++ Hangman.py
@@ -37,3 +37,45 @@ if name == hidden:
     print("You survived!")
 else:
     print("You are hanged!")
+
+
+# Stage 5
+
+import random
+
+print("H A N G M A N")
+print()
+
+wordlist = ['python', 'java', 'kotlin', 'javascript']
+pattern = random.choice(wordlist)
+random_word = list(pattern)
+hyphens_num = list('-' * len(random_word))
+
+hyphens_count = hyphens_num.count('-')
+
+hyphens_join = ''.join(hyphens_num)
+
+tries = 0
+while tries < 8:
+
+    tries += 1
+    print(hyphens_join)
+    a = input('Input a letter: ')
+
+    i = random_word.count(a)
+    if i == 0 and hyphens_num.count('-') > 0:
+        print('No such letter in the word')
+        print()
+
+    else:
+        while i != 0:
+            b = random_word.index(a)
+            hyphens_num[b] = a
+            random_word[b] = '-'
+            i -= 1
+            hyphens_count = hyphens_num.count('-')
+        hyphens_join = ''.join(hyphens_num)
+        print()
+
+print("""Thanks for playing!
+We'll see how well you did in the next stage """)
