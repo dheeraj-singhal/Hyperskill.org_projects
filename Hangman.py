b372a79677aa92fc50c29511b6cedce17a1cd522
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-05-03 17:49:41 +0530

    Stage 7 implemented successfully

--- Hangman.py
+++ Hangman.py
@@ -130,3 +130,61 @@ elif tries == 0 and hyphens_count == 0:
         print("You survived!")
 else:
         print("You are hanged!")
+
+
+# Stage 7
+
+import random
+
+print("H A N G M A N")
+
+wordlist = ['python', 'java', 'kotlin', 'javascript']
+pattern = random.choice(wordlist)
+random_word = list(pattern)
+hyphens_num = list('-' * len(random_word))
+
+hyphens_count = hyphens_num.count("-")
+hyphens_join = ''.join(hyphens_num)
+
+tries = 8
+selected = set()
+
+while hyphens_count > 0 and tries > 0:
+    print()
+    print(hyphens_join)
+    guess = input("Input the letter: ")
+    if len(guess) == 1 and guess.isalpha() and guess.islower():
+        if guess in pattern and guess not in selected:
+            for i in range(len(pattern)):
+                if guess == pattern[i]:
+                    hyphens_num[i] = pattern[i]
+                    hyphens_count -= 1
+                    if guess not in selected:
+                        selected.add(guess)
+
+        elif guess in selected:
+
+            if tries >= 0:
+                print("You already typed this letter")
+
+        elif tries > 0 and guess not in pattern:
+            print("No such letter in the word")
+            tries -= 1
+            if guess not in selected:
+                selected.add(guess)
+        hyphens_join = "".join(hyphens_num)
+    elif len(guess) > 1:
+        print("You should print a single letter")
+    else:
+        print("It is not an ASCII lowercase letter")
+
+if tries != 0 and hyphens_count == 0:
+    print(pattern)
+    print("You guessed the word!")
+    print("You survived!")
+elif tries == 0 and hyphens_count == 0:
+    print(pattern)
+    print("You guessed the word!")
+    print("You survived!")
+else:
+    print("You are hanged!")
