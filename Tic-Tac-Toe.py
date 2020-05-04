d80b8a0922d1598ed35650afa35c26f72ec71deb
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-05-04 10:05:16 +0530

    Stage 2 implemented successfully

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -3,3 +3,13 @@
 print('''X O X
 O X O
 X X O''')
+
+
+# Stage 2
+
+s = input()
+print('---------')
+print("|",s[0],s[1],s[2],"|")
+print("|",s[3],s[4],s[5],"|")
+print("|",s[6],s[7],s[8],"|")
+print('---------')
