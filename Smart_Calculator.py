b4c5a450cabfb633c8bbf77cc9079a65c8697186
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-05-07 19:44:58 +0530

    Stage 3 implemented successfully

--- Smart_Calculator.py
+++ Smart_Calculator.py
@@ -10,3 +10,19 @@ c = 0
 for i in a:
     c += int(i)
 print(c)
+
+
+# Stage 3
+
+while True:
+    a = input()
+    if a == "":
+        continue
+    elif a == "/help":
+        print("The program calculates the sum of numbers")
+    elif a == "/exit":
+        print("Bye!")
+        break
+    else:
+        ls = [int(i) for i in a.split(" ")]
+        print(int(sum(ls)))
