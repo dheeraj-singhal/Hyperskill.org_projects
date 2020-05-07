0abda044917ca9d3c18012f22751857fd4eb3106
dheeraj-singhal <dheeraj.gla_cs19@gla.ac.in>
2020-05-07 19:51:00 +0530

    Stage 2 implemented successfully

--- Smart_Calculator.py
+++ Smart_Calculator.py
@@ -1,15 +1,24 @@
 # Stage 1
 
-print(sum(map(int, input().split())))
-
-
-# Stage 2
-
 a = input().split(' ')
 c = 0
 for i in a:
     c += int(i)
 print(c)
+
+
+# Stage 2
+
+while True:
+    a = input()
+    if a == "":
+        continue
+    elif a == "/exit":
+        print("Bye!")
+        break
+    else:
+        ls = [int(i) for i in a.split(" ")]
+        print(int(sum(ls)))
 
 
 # Stage 3
